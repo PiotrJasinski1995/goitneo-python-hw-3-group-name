9df7eb8171c347c590e10238b7d423d7500406e6
PiotrJasinski1995 <piotr.jasinski1995@gmail.com>
2024-03-25 00:13:22 +0100

    feat: add birthday commands and add saving data to file

--- address_book.py
+++ address_book.py
@@ -6,9 +6,12 @@ class PhoneFormatException(Exception):
     pass
 
 
-class ContactExistsError(Exception):
+class DateFormatException(Exception):
     pass
 
+
+class ContactExistsError(Exception):
+    pass
 
 
 class Field:
@@ -34,16 +37,31 @@ class Phone(Field):
         super().__init__(value)
 
 
+class Birthday(Field):
+    def __init__(self, value):
+        match = re.fullmatch('\\d{2}.\\d{2}.\\d{4}$', value)
+
+        # Exception for future error handling
+        if match == None:
+            raise DateFormatException
+
+        super().__init__(value)
+
+
 class Record:
     def __init__(self, name):
         self.name = Name(name)
         self.phone = ''
+        self.birthday = None
 
     def __str__(self):
         return f'Contact name: {self.name.value}, phone: {self.phone}'
 
     def add_phone(self, phone):
         self.phone = Phone(phone)
+
+    def add_birthday(self, birthday):
+        self.birthday =  Birthday(birthday)
 
 
 class AddressBook(UserDict):
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from address_book import *
+from birthday_function import get_birthday_per_week
+from datetime import datetime
+import pickle
+from pathlib import Path
 
 
 def parse_input(user_input):
@@ -8,7 +12,7 @@ def parse_input(user_input):
     return cmd, *args
 
 
-def print_phonebook_list(contacts):
+def get_phonebook_list(contacts):
     contact_name = 'Contact'
     max_key_len = max(len(key) for key in contacts)
     max_key_len = len(contact_name) if len(contact_name) > max_key_len else max_key_len
@@ -38,19 +42,23 @@ def print_phonebook_list(contacts):
     return contacts_string
 
 
+def get_birthday_contact(contact):  
+    return {'name': contact.name.value, 'birthday': datetime.strptime(contact.birthday.value, '%d.%m.%Y')}
+
+
 def input_error(func):
     def inner(args, kwargs):
         try:
             return func(args, kwargs)
         except ValueError:
-            return '\nWrong arguments!\n'\
-                'For "add" and "change" give me name and phone (10 digits).\n'\
-                'For "phone" give me name.\n'\
-                'For "all" give me no arguments.\n'
+            return 'Wrong arguments!\n'\
+                'Type "help" for more info.'
         except KeyError:
             return 'Contact does not exists!'
         except PhoneFormatException:
             return 'Number should contain 10 digits!'
+        except DateFormatException:
+            return 'Date should be given in DD.MM.YYYY format!'
         except ContactExistsError:
             return 'Contact already exists!'
     return inner
@@ -92,12 +100,83 @@ def get_all(args, contacts):
     if not contacts:
         return 'No contacts in phonebook!!!'
     
-    return print_phonebook_list(contacts)
+    return get_phonebook_list(contacts)
+
+
+@input_error
+def add_birthday(args, contacts):
+    name, birthday = args
+    contacts[name].birthday = Birthday(birthday)
+    
+    return f'Birthday for {name} added.'
+
+
+def get_birthday(args, contacts):
+    (name,) = args
+
+    if contacts[name].birthday == None:
+        return 'No birthday added for this contact!'
+    
+    return f'Birthday for {name}: {contacts[name].birthday}'
+
+  
+def get_birthdays(args, contacts):
+    if len(args) != 0:
+        return 'You should not put any arguments in "birthdays" command!'
+    
+    if not contacts:
+        return 'No contacts in phonebook!!!'
+    
+    filter_list = filter(lambda contact: contact.birthday != None, contacts.data.values())
+    contact_dict = map(get_birthday_contact, filter_list)
+    return get_birthday_per_week(contact_dict)
+
+
+def save_address_book(contacts):
+    file_name = 'address_book.bin'
+
+    with open(file_name, 'wb') as file:
+        pickle.dump(contacts, file)
+
+
+def load_address_book():
+    file_name = 'address_book.bin'
+    file_path = Path(f'./{file_name}')
+
+    if file_path.is_file():
+        with open(file_name, 'rb') as file:
+            return pickle.load(file)
+    
+    return AddressBook()
+
+
+def help():
+    commands = ['Command', '-' * 14, 'add', 'change', 'phone', 'all', 'add-birthday', 'show-birthday', 'birthdays', 'hello', 'close or exit']
+    arguments = ['Arguments', '-' * 20, '[name] [phone]', '[name] [phone]', '[name] [new phone]', '[name]', 'no arguments', '[name]', 'no arguments', 'no arguments', 'no arguments']
+    texts = ['Help text',
+             '-' * 10,
+             'Add a new contact with a name and phone number.',
+             'Change the phone number for the specified contact.',
+             'Show the phone number for the specified contact.',
+             'Show all contacts in the address book.',
+             'Add a date of birth for the specified contact.',
+             'Show the date of birth for the specified contact.',
+             'Show birthdays that will take place within the next week.',
+             'Receive a greeting from a bot.',
+             'Close the app.']
+    
+    help_text = '\n'
+
+    for i in range(len(commands)):
+        help_text += '{command:<14} {argument:<20} {text}\n'.format(command=commands[i], argument=arguments[i], text=texts[i])
+
+    return help_text
 
 
 def main():
-    contacts = AddressBook()
+    contacts = load_address_book()
     print('Welcome to the assistant bot!')
+
     while True:
         try:
             user_input = input('Enter a command: ')
@@ -116,10 +195,20 @@ def main():
                 print(get_phone(args, contacts))
             elif command == 'all':
                 print(get_all(args, contacts))
+            elif command == 'add-birthday':
+                print(add_birthday(args, contacts))
+            elif command == 'show-birthday':
+                print(get_birthday(args, contacts))
+            elif command == 'birthdays':
+                print(get_birthdays(args, contacts))
+            elif command == 'help':
+                print(help())
             else:
                 print('Invalid command.')   
         except ValueError:
             print('Please use commands!')
+
+    save_address_book(contacts)
 
 
 if __name__ == '__main__':
